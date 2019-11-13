14bc210541ea1a36b4456755a3e0efe7cb4e4ecf
Mystikal57 <8256583+Mystikal57@users.noreply.github.com>
2019-11-13 16:04:10 +0100

    Update parametres.php

--- parametres.php
+++ parametres.php
@@ -4,6 +4,8 @@ $access_token = "VOTRE_ACCESS_TOKEN";
 $site_id = "VOTRE_SITE_ID";
 $device_id = "VOTRE_DEVICE_ID";
 $label_siren = "NOM_DE_VOTRE_SIRENE_INTERIEURE";
-$delais_activation = 35; #delais d'activation de votre alarme +5 secs 
+$infos_user = "https://api.myfox.io/v3/user?access_token=".$access_token;
+$details_user = json_decode(file_get_contents($infos_user));
+$delais_activation = ($details_user->sites[0]->exit_delay)+5; #delais d'activation de votre alarme +5 secs
 
 ?>
