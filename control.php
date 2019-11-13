9f1cc998f86f90e22171b8a9a0e8a79eaf0539f0
Mystikal57 <8256583+Mystikal57@users.noreply.github.com>
2019-11-13 11:41:31 +0100

    Update control.php

--- control.php
+++ control.php
@@ -1,9 +1,5 @@
 <?php
-
-$access_token = "VOTRE_ACCESS_TOKEN";
-$site_id = "VOTRE_SITE_ID";
-$device_id = "VOTRE_DEVICE_ID";
-$label_siren = "NOM_DE_VOTRE_SIRENE_INTERIEURE";
+include_once('parametres.php');
 $action = $_GET["action"];
 $delais_activation = 35; #delais d'activation de votre alarme +5 secs 
 
