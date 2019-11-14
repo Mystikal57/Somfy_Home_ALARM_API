bca07180c89301cf58458c442477b7c4e32fde0f
Mystikal57 <8256583+Mystikal57@users.noreply.github.com>
2019-11-14 14:46:38 +0100

    Update parametres.php

--- parametres.php
+++ parametres.php
@@ -1,11 +1,30 @@
 <?php
+session_start();
 
-$access_token = "VOTRE_ACCESS_TOKEN";
-$site_id = "VOTRE_SITE_ID";
-$device_id = "VOTRE_DEVICE_ID";
-$label_siren = "NOM_DE_VOTRE_SIRENE_INTERIEURE";
+$log_level = 1;
+$config = parse_ini_file('../private/config.ini');
+$username = $config['username'];
+$password = $config['password'];
+$client_id = $config['client_id'];
+$client_secret = $config['client_secret'];
+$access_token = $_SESSION["access_token"];
+$refresh_token = $_SESSION["refresh_token"];
+
+
+if ($_SESSION["site_id"] == ""){
 $infos_user = "https://api.myfox.io/v3/user?access_token=".$access_token;
 $details_user = json_decode(file_get_contents($infos_user));
 $delais_activation = ($details_user->sites[0]->exit_delay)+5; #delais d'activation de votre alarme +5 secs
+$_SESSION["site_id"] = $details_user->sites[0]->site_id;
+}
+$site_id = $_SESSION["site_id"];
+
+
+if ($_SESSION["device_id"] == ""){
+$infos_devices = "https://api.myfox.io/v3/site/".$site_id."/device?access_token=".$access_token;
+$details_devices = json_decode(file_get_contents($infos_devices));
+$_SESSION["device_id"] = $details_devices->items[1]->device_id;
+$_SESSION["label_siren"] = $details_devices->items[1]->label;
+}
 
 ?>
