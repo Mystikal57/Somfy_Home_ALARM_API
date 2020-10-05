4adfbbc5ffa5578f53db9970e40b9fa74855874e
Mystikal57 <8256583+Mystikal57@users.noreply.github.com>
2020-10-05 17:10:12 +0200

    Update state.php

--- state.php
+++ state.php
@@ -1,7 +1,8 @@
 <?php
 require_once('auth.php');
 require('parametres.php');
-$url_state = "https://api.myfox.io/v3/site/".$site_id."?access_token=".$access_$
+$url_state = "https://api.myfox.io/v3/site/".$site_id."?access_token=".$access_token;
+
 
 $state = json_decode(file_get_contents($url_state));
 echo $state->security_level;
